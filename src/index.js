0f681a71c8e0b3ac6cbb902113cdaf29b2f1b242
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-10-31 02:29:15 +0800

    Integrasi.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router from './navigation/Router'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
