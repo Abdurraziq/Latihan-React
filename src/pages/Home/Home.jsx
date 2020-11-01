1a7191af9688c0fc54b8c589989752aa63c39464
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-01 23:36:00 +0800

    Redesign some pages.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { Container } from "react-bootstrap";
-import CarouselApp from "../../components/CarouselApp/CarouselApp";
+import { CarouselApp } from "../../components";
 
 export default class Home extends Component {
   render() {
     return (
-      <Fragment>
+      <Container className="mt-4">
         <CarouselApp
           items={[
             {
@@ -25,10 +25,91 @@ export default class Home extends Component {
             },
           ]}
         />
-        <Container className="mt-4">
-          <h2 className="font-weight-bold mb-2">Daftar Produk</h2>
-        </Container>
-      </Fragment>
+        <h2 className="font-weight-bold mt-5 mb-2">DigiStore</h2>
+        <p>Selamat datang di Digistore.</p>
+        <h4>Lorem Ipsum</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo animi
+          eos, praesentium molestias dolor obcaecati corrupti fuga. Ea, magnam
+          nam incidunt accusantium debitis esse ex quod, itaque, facere deleniti
+          minus. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Quibusdam qui aperiam ullam porro et quae id, deleniti quidem
+          reprehenderit ipsum ipsa reiciendis. Quis, architecto? Repellendus
+          voluptatem aut harum architecto repudiandae! Id aliquid eveniet
+          possimus voluptate nulla porro ullam ad! Fuga praesentium veritatis
+          tenetur vero doloribus? Recusandae assumenda, odio dicta saepe
+          deleniti quisquam vero dolores odit molestias quia natus rem qui!
+          Nihil quia error voluptatum dicta ea expedita possimus itaque,
+          inventore nemo repellendus cum excepturi accusamus temporibus nobis
+          obcaecati veritatis quidem enim tenetur et modi officia molestiae
+          fugit animi. Ullam, commodi. Earum dicta, ut quos sint tempore
+          cupiditate voluptates ipsa ipsum blanditiis debitis quas aperiam,
+          cumque architecto voluptatibus velit. Incidunt voluptatum delectus
+          quis magnam porro laboriosam voluptates, velit labore repellendus
+          ipsum! Tenetur odit suscipit illum repellat impedit expedita magnam
+          iusto voluptate! Repellat laborum similique molestiae a obcaecati odit
+          magni ratione, exercitationem nulla quos dicta, ab atque animi minus.
+          Ab, incidunt cupiditate.
+        </p>
+        <h4>Lorem Ipsum</h4>
+        <img
+          className="img-fluid mb-3"
+          src="https://wallpaperaccess.com/download/light-gray-1109072"
+          alt="gambar"
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo animi
+          eos, praesentium molestias dolor obcaecati corrupti fuga. Ea, magnam
+          nam incidunt accusantium debitis esse ex quod, itaque, facere deleniti
+          minus. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Quibusdam qui aperiam ullam porro et quae id, deleniti quidem
+          reprehenderit ipsum ipsa reiciendis. Quis, architecto? Repellendus
+          voluptatem aut harum architecto repudiandae! Id aliquid eveniet
+          possimus voluptate nulla porro ullam ad! Fuga praesentium veritatis
+          tenetur vero doloribus? Recusandae assumenda, odio dicta saepe
+          deleniti quisquam vero dolores odit molestias quia natus rem qui!
+          Nihil quia error voluptatum dicta ea expedita possimus itaque,
+          inventore nemo repellendus cum excepturi accusamus temporibus nobis
+          obcaecati veritatis quidem enim tenetur et modi officia molestiae
+          fugit animi. Ullam, commodi. Earum dicta, ut quos sint tempore
+          cupiditate voluptates ipsa ipsum blanditiis debitis quas aperiam,
+          cumque architecto voluptatibus velit. Incidunt voluptatum delectus
+          quis magnam porro laboriosam voluptates, velit labore repellendus
+          ipsum! Tenetur odit suscipit illum repellat impedit expedita magnam
+          iusto voluptate! Repellat laborum similique molestiae a obcaecati odit
+          magni ratione, exercitationem nulla quos dicta, ab atque animi minus.
+          Ab, incidunt cupiditate.
+        </p>
+        <h4>Lorem Ipsum</h4>
+        <img
+          className="img-fluid mb-3"
+          src="https://wallpaperaccess.com/download/light-gray-1109072"
+          alt="gambar"
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo animi
+          eos, praesentium molestias dolor obcaecati corrupti fuga. Ea, magnam
+          nam incidunt accusantium debitis esse ex quod, itaque, facere deleniti
+          minus. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Quibusdam qui aperiam ullam porro et quae id, deleniti quidem
+          reprehenderit ipsum ipsa reiciendis. Quis, architecto? Repellendus
+          voluptatem aut harum architecto repudiandae! Id aliquid eveniet
+          possimus voluptate nulla porro ullam ad! Fuga praesentium veritatis
+          tenetur vero doloribus? Recusandae assumenda, odio dicta saepe
+          deleniti quisquam vero dolores odit molestias quia natus rem qui!
+          Nihil quia error voluptatum dicta ea expedita possimus itaque,
+          inventore nemo repellendus cum excepturi accusamus temporibus nobis
+          obcaecati veritatis quidem enim tenetur et modi officia molestiae
+          fugit animi. Ullam, commodi. Earum dicta, ut quos sint tempore
+          cupiditate voluptates ipsa ipsum blanditiis debitis quas aperiam,
+          cumque architecto voluptatibus velit. Incidunt voluptatum delectus
+          quis magnam porro laboriosam voluptates, velit labore repellendus
+          ipsum! Tenetur odit suscipit illum repellat impedit expedita magnam
+          iusto voluptate! Repellat laborum similique molestiae a obcaecati odit
+          magni ratione, exercitationem nulla quos dicta, ab atque animi minus.
+          Ab, incidunt cupiditate.
+        </p>
+      </Container>
     );
   }
 }
