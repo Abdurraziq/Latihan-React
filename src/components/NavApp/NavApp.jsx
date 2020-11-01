7773fa623f8b860f7349ce8a6d9d82feb11b64db
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-01 23:38:46 +0800

    Add login/logout button.

--- src/components/NavApp/NavApp.jsx
+++ src/components/NavApp/NavApp.jsx
@@ -1,40 +1,44 @@
-import React, { Component } from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import React, { useContext } from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { authContext } from "../../context/AuthContext";
 
-export default class NavApp extends Component {
-  render() {
-    const { navItems } = this.props;
+const NavApp = (props) => {
+  const { dataLogin, logout } = useContext(authContext);
+  const { navItems } = props;
 
-    return (
-      <Navbar
-        collapseOnSelect
-        expand="md"
-        bg="dark"
-        variant="dark"
-        sticky="top"
-        className="shadow-sm"
-      >
-        <Container>
-          <Navbar.Brand href="/">DigiStore</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbar-nav" />
-          <Navbar.Collapse id="navbar-nav">
-            <Nav className="ml-auto">
-              {navItems.map((item, index) => (
-                <Nav.Link
-                  as={NavLink}
-                  exact
-                  to={item.link}
-                  eventKey={item.label}
-                  key={index}
-                >
-                  {item.label}
-                </Nav.Link>
-              ))}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
-}
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="md"
+      bg="dark"
+      variant="dark"
+      sticky="top"
+      className="shadow-sm"
+    >
+      <Container>
+        <Navbar.Brand href="/">DigiStore</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ml-auto">
+            {navItems.map((item, index) => (
+              <Nav.Link as={NavLink} exact to={item.link} key={index}>
+                {item.label}
+              </Nav.Link>
+            ))}
+          </Nav>
+          <Button
+            variant="primary"
+            size="sm"
+            className="ml-3"
+            onClick={() => logout()}
+          >
+            {dataLogin ? "Logout" : "Login"}
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavApp;
