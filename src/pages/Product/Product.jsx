3c58bc9cad775a3ec1c3f107175a1decf8d39557
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-01 23:32:31 +0800

    Move from class to function component  to work with hooks.

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,81 +1,78 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
-import CardApp from "../../components/CardApp/CardApp";
-import LoadIndicator from "../../components/LoadIndicator/LoadIndicator";
-import ButtonApp from "../../components/ButtonApp/ButtonApp";
+import { ButtonApp, CardApp, LoadIndicator } from "../../components";
 
-export default class ListProduk extends Component {
-  constructor() {
-    super();
-    this.state = {
-      length: 0,
-      products: [],
-    };
-  }
 
-  componentDidMount() {
+const Product = () => {
+  const [length, setLength] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((res) =>
-        this.setState({
-          length: res.length,
-          products: res,
-        })
-      )
+      .then((res) => {
+        setLength(res.length);
+        setProducts(res);
+      })
       .catch((e) => {
         console.log(e);
       });
-  }
+  }, []);
 
-  render() {
-    const { products, length } = this.state;
-    const productItem = products.map((product) => (
-      <CardApp
-        key={product.id}
-        img={product.image}
-        title={product.title}
-        text={product.description}
-        action={
-          <Fragment>
-            <ButtonApp
-              btn={{
-                variant: "outline-primary",
-                className: "flex-fill mr-1",
-                size: "sm",
-              }}
-              icon="fas fa-shopping-cart"
-              title="Beli"
-            />
-            <ButtonApp
-              btn={{
-                variant: "outline-danger",
-                className: "flex-fill mr-1",
-                size: "sm",
-              }}
-              icon="fas fa-shopping-cart"
-              title="Favorite"
-            />
-          </Fragment>
-        }
-      />
-    ));
+  const productItem = products.map((product) => (
+    <CardApp
+      key={product.id}
+      img={product.image}
+      title={product.title}
+      text={product.description}
+      action={
+        <Fragment>
+          <ButtonApp
+            btn={{
+              variant: "outline-primary",
+              className: "flex-fill mr-1",
+              size: "sm",
+              onClick: () => {
+                alert(`Anda membeli ${product.title}`);
+              },
+            }}
+            icon="fas fa-shopping-cart"
+            title="Beli"
+          />
+          <ButtonApp
+            btn={{
+              variant: "outline-danger",
+              className: "flex-fill mr-1",
+              size: "sm",
+              onClick: () => {
+                alert(`Anda memfavoritkan ${product.title}`);
+              },
+            }}
+            icon="fas fa-shopping-cart"
+            title="Favorite"
+          />
+        </Fragment>
+      }
+    />
+  ));
 
-    return (
-      <Container className="mt-4">
-        <h2 className="font-weight-bold mb-2">Daftar Produk</h2>
-        <p className="font-italic text-muted mb-4">
-          Produk yang ditampilkan berikut ini hanya berupa data dummy yang
-          diambil dari <a href="https://fakestoreapi.com/">fakestoreapi</a>
-        </p>
+  return (
+    <Container className="mt-4">
+      <h2 className="font-weight-bold mb-2">Daftar Produk</h2>
+      <p className="font-italic text-muted mb-4">
+        Produk yang ditampilkan berikut ini hanya berupa data dummy yang diambil
+        dari <a href="https://fakestoreapi.com/">fakestoreapi</a>
+      </p>
 
-        {length ? (
-          <Row xs={1} md={2} lg={3}>
-            {productItem}
-          </Row>
-        ) : (
-          <LoadIndicator />
-        )}
-      </Container>
-    );
-  }
-}
+      {length ? (
+        <Row xs={1} md={2} lg={3}>
+          {productItem}
+        </Row>
+      ) : (
+        <LoadIndicator />
+      )}
+    </Container>
+  );
+};
+
+export default Product;
